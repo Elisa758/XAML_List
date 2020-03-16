05079be57c66ba9d6a5c5b1a55c827be6f506b53
Elisa ROMAIN <romain.elisa@yahoo.fr>
2020-03-16 16:55:04 +0100

    first commit

---
+++ XAML_WPF_Quest2/XAML_WPF_Quest2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XAML_WPF_Quest2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddInfectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            string nameAndBehavior = String.Format("{0} | {1} ", nameOfInfected.Text, Behavior.Text);
+            if (nameOfInfected.Text == String.Empty || listInfected.Items.Contains(nameAndBehavior))
+            {
+                MessageBox.Show("Something went wrong ! ");
+            }
+            else
+            {
+                listInfected.Items.Add(nameAndBehavior);
+        
+            }
+        }
+    }
+}
